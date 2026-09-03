d715668871e533f06a975852632519eff2f4a7bf
Nile Miller <nmiller@galileosuite.com>
2026-09-03 14:38:33 -0400

    add toast messages

---
+++ src/lib/toast.ts
@@ -0,0 +1,33 @@
+export const correctMessages = [
+  'Nice!', 
+  'Got it!', 
+  'Got \'em!', 
+  'Groovy!', 
+  'Awesome!', 
+  'Sweet!', 
+  'That\'s the spirit!',
+  'Lit!',
+  'Legitness!',
+  'That\'s it!',
+  'You got it!',
+  'Très bien!',
+  '¡Qué chulo!', 
+  'Smarty pants!',
+  'Capital!',
+  'Copacetic!'
+]; 
+
+export const incorrectMessages = [
+  'Nah...',
+  'Not quite...',
+  'Try again...', 
+  'Nope!', 
+  'Hmm...',
+  'That\'s not quite right...', 
+  'Swing and a miss!',
+  'Oof!', 
+  'Close, but no cigar...',
+  'Guess again!'
+]
+
+export const oneAwayMessage = 'One away!'
